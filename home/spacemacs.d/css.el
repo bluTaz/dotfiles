6bb06d3b967167a91a545d6d667f3029fcac64f8
David J Galvan Jr <djgalvan01@gmail.com>
2018-04-16 13:42:24 -0400

    Removed old emacs files

--- home/spacemacs.d/css.el
+++
@@ -1,7 +0,0 @@
-(add-hook 'css-mode-hook 'rainbow-mode)
-
-;; (add-hook 'css-mode-hook
-;;           '(lambda ()
-;;              (add-hook 'after-save-hook
-;;                        (lambda () (when (executable-find "csstidy") (css-check))) nil t)
-;;              (rainbow-mode 1)))
